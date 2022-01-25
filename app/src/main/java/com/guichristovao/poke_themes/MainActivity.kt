6a1a3728c7197537e71221c4576503c1b04070bd
Guilherme de Sá Christovão <guildschris@gmail.com>
2022-01-24 22:38:53 -0300

    :tada: Initial commit

---
+++ app/src/main/java/com/guichristovao/poke_themes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.guichristovao.poke_themes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    val gradleVersion = "7.0.4"
+    val kotlinVersion = "1.6.0"
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:$gradleVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
