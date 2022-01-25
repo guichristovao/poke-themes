61431463b5bcea46ff2b72197eb9ac7943ecbd4e
Guilherme de Sá Christovão <guildschris@gmail.com>
2022-01-25 01:28:27 -0300

    :sparkles: Add blue theme

---
+++ theme/brand/blue/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(Configurations.compileSdkVersion)
+
+    defaultConfig {
+        minSdkVersion(Configurations.minSdkVersion)
+        targetSdkVersion(Configurations.targetSdkVersion)
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":theme:base"))
+
+    implementation("com.google.android.material:material:1.5.0")
+}
