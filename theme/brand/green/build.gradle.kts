1984d95e46ec99f1136d9e682017525e2a0daf09
Guilherme de Sá Christovão <guildschris@gmail.com>
2022-01-27 23:30:59 -0300

    :sparkles: Add green brand theme

--- theme/brand/green/build.gradle.kts
+++ theme/brand/green/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     kotlin("android")
 }
 
@@ -7,11 +7,8 @@ android {
     compileSdkVersion(Configurations.compileSdkVersion)
 
     defaultConfig {
-        applicationId = Configurations.applicationId
         minSdkVersion(Configurations.minSdkVersion)
         targetSdkVersion(Configurations.targetSdkVersion)
-        versionCode = Configurations.versionCode
-        versionName = Configurations.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -35,15 +32,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":theme:brand:blue"))
-    implementation(project(":theme:brand:green"))
-    implementation(project(":theme:brand:red"))
+    implementation(project(":theme:base"))
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    testImplementation("junit:junit:4.+")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
